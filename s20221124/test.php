46434248d7a7ef312b8b41902246e0cebff19a9f
ruzs <eddie10913@gmail.com>
2022-11-24 14:07:55 +0800

    for loop 9*9 & input prompt

---
+++ s20221124/test.php
@@ -0,0 +1,14 @@
+<?php 
+    $variable = "hello";
+    // php .
+    $str = "abc" . $variable . "efg";
+
+    // output abchelloefg
+?>
+
+<script>
+    let variable_js = 'helloJs';
+    // js +
+    str_js = "abc" + variable_js + "efg";
+
+</script>
